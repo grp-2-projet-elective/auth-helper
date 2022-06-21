904622863095842450c572cd949da42c395c2578
felicienfouillet <felicienfouillet@live.fr>
2022-06-21 08:18:24 +0200

    (lint): fix some lint issues

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,17 +2,17 @@ import { createHmac } from 'crypto';
 
 export abstract class AuthMiddleware {
     public static verifyAccessToken(req, res, next) {
-        const accessToken: string = req.headers["x-access-token"] as string;
+        const accessToken: string = req.headers['x-access-token'] as string;
 
         if (!accessToken) {
-            return res.status(403).send({ message: "Auth token not provided!" });
+            return res.status(403).send({ message: 'Auth token not provided!' });
         }
 
         const validHashedToken = createHmac('sha256', process.env.TOKEN_SECRET as string).update(process.env.ACCESS_TOKEN as string).digest('hex');
         const hashedToken = createHmac('sha256', process.env.TOKEN_SECRET as string).update(accessToken).digest('hex');
 
         if (hashedToken !== validHashedToken) {
-            return res.status(403).send({ message: "Auth token invalid!" });
+            return res.status(403).send({ message: 'Auth token invalid' });
         }
 
         return next();
