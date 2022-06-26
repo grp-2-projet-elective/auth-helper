4a2127d25aa0e7fd688401686766f2046ba80078
Félicien Fouillet <50355349+felicienfouillet@users.noreply.github.com>
2022-06-26 22:51:55 +0200

    8 etq dev jv ajouter les types du backend p faciliter le dev des api (#9)
    * (evo): add helpers and models form backend apis
    
    * 1.1.0
    
    * (fix): fix exports
    
    * 1.1.1

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,13 +1,14 @@
+import { Roles } from "./users.model";
+
 export interface Tokens {
   accessToken: string;
   refreshToken: string;
 }
 
-export enum Roles {
-  CUSTOMER = 1,
-  RESTAURANT_OWNER = 2,
-  DELIVERY_MAN = 3,
-  TECHNICAL_DEPARTMENT = 4,
-  COMERCIAL_DEPARTMENT = 4,
-  EXTERNAL = 6,
+export interface TokenData {
+  id: number;
+  mail: string;
+  role: Roles;
+
+  restaurantId?: number;
 }
