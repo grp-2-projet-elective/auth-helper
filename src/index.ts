93e9573d099cc84991dd02f6164b99a8737479c0
felicienfouillet <felicienfouillet@live.fr>
2022-06-26 22:48:50 +0200

    (fix): fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+export * from './handlers/exceptions.handler';
+export * from './handlers/unknown-routes.handler';
 export * from './middlewares/auth.middleware';
+export * from './models/api-exceptions.model';
 export * from './models/auth.model';
+export * from './models/users.model';
+export * from './utils/exceptions';
+
