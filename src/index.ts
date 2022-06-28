900f33a16edde17776e0459a0e84238c8b6d1a1c
felicienfouillet <felicienfouillet@live.fr>
2022-06-28 08:18:47 +0200

    (fix): fix logger export

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './middlewares/auth.middleware';
 export * from './models/api-exceptions.model';
 export * from './models/auth.model';
 export * from './models/users.model';
+export * from './services/logger.service';
 export * from './utils/exceptions';
 
